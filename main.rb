b81b2f6f54024c6c94686f36352b8f1e84d3f20c
iLynette <lynetteacholah@icloud.com>
2022-05-23 17:22:58 +0300

    Refactor main

--- main.rb
+++ main.rb
@@ -1,50 +1,45 @@
-require './app'
+require_relative './app'
 
-class Main
-  def self.home_page
-    puts 'Please enter a number: '
+puts 'Welcome to your school library'
 
-    @content = {
-      '1' => 'List all books',
-      '2' => 'List all people',
-      '3' => 'Create a person (teacher or student)',
-      '4' => 'Create a book',
-      '5' => 'Create a rental',
-      '6' => 'List all rentals for a given person id',
-      '7' => 'Exit'
-    }
-    @content.each { |index, string| puts "#{index} - #{string}" }
-
-    Integer(gets.chomp)
-  end
-
-  app = App.new
-
-  loop do
-    case home_page
-    when 1
-      app.booklist
-    when 2
-      app.peoplelist
-    when 3
-      app.create_person
-    when 4
-      app.create_book
-    when 5
-      app.create_rental
-    when 6
-      app.rentalslist
-    when 7
-      puts 'goodbuy!'
-      exit
-    else
-      puts 'out of range input!'
-    end
-  end
+def list_selections
+  puts ''
+  puts "Please choose an option by selecting a number:
+              1. List all books
+              2. List all people
+              3. Create person account
+              4. Create a book
+              5. Create a rental
+              6. List all rentals for a given person ID
+              7. Exit"
+  gets.chomp
 end
 
 def main
-  App.new
+  app = App.new
+  app.start
 end
 
-Main
+def selection
+  case list_selections
+  when '1'
+    @books.booklist
+  when '2'
+    peoplelist
+  when '3'
+    create_person
+  when '4'
+    @books.create_book
+  when '5'
+    create_rental
+  when '6'
+    rentalslist
+  when '7'
+    puts 'Thank you for using the app. Goodbye!'
+  else
+    puts 'Invalid input. Try again'
+  end
+  selection
+end
+
+main
