f848a7ffa61e41bbfce750f86f8811d2115fdf01
ZahraArshia <78906545+ZahraArshia@users.noreply.github.com>
2022-05-24 18:55:42 +0430

    implement people data reservation

--- data.rb
+++ data.rb
@@ -3,42 +3,7 @@ require './student'
 require './teacher'
 require './rental'
 
-# class Data
-#   def initialize(datafile)
-#     @datafile = datafile
-#     @file = "data/#{@datafile}.json"
-#     write([]) unless File.file?(@file)
-#   end
-
-#   def read
-#     if File.exists?(@file)
-#       data = File.read(@file)
-#       JSON.parse(data)
-#     else
-#       File.open("data/#{@datafile}.json", 'w')
-#       puts 'new source created'
-#     end
-#   end
-
-#   def write(data)
-#     if File.exists?(@file)
-#       json = JSON.generate(data)
-#       File.write(@file, json, mode: "a")
-#     else
-#       File.open("data/#{@datafile}.json", 'w')
-#       puts 'new source created'
-#     end
-#   end
-# end
-
-# def create_file(name)
-#   File.write("./data/#{name}.json", []) unless File.exist?("./data/#{name}.json")
-# end
-
 def initialize_files
-  # create_file(book_file)
-  # create_file(people_file)
-  # create_file(rental_file)
   File.write('./data/book_file.json', []) unless File.exist?('./data/book_file.json')
   File.write('./data/people_file.json', []) unless File.exist?('./data/people_file.json')
   File.write('./data/rental_file.json', []) unless File.exist?('./data/rental_file.json')
@@ -53,9 +18,6 @@ def read_books
 end
 
 def write_books(books)
-  # books.each do |book|
-  #   File.write('./data/book_file.json', book, mode: "a")
-  # end
   temp = []
   books.each do |book|
     temp.push( {
@@ -66,3 +28,44 @@ def write_books(books)
 
   File.write('./data/book_file.json', (JSON.generate(temp)).to_s)
 end
+
+def write_people(persons)
+  temp = []
+  persons.each do |person|
+    temp << if person.instance_of? Student
+      {
+        role: 'Student',
+        name: person.name,
+        age: person.age,
+        parent_permission: person.parent_permission
+      }
+    elsif person.instance_of? Teacher
+      {
+        role: 'Teacher',
+        name: person.name,
+        age: person.age,
+        specialization: person.specialization
+      }
+    end
+  end
+
+  ruby = JSON.generate(temp)
+
+  File.write('./data/people_file.json', ruby.to_s)
+end
+
+def read_people
+  people = []
+  (JSON.parse(File.read('./data/people_file.json'))).each do |person|
+    people << case person['role']
+    when 'Student'
+      Student.new(person['age'], person['name'], parent_permission: person['parent_permission'])
+    when 'Teacher'
+      Teacher.new(person['age'], person['specialization'], person['name'])
+    end
+  end
+  people
+end
+
+
+
