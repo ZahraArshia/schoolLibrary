0938817ad81fde289a2aff833edf31cd6ec6387f
iLynette <lynetteacholah@icloud.com>
2022-05-25 22:47:06 +0300

    Modify the book.rb file

--- book.rb
+++ book.rb
@@ -10,4 +10,9 @@ class Book
   def add_rental(person, date)
     Rental.new(date, self, person)
   end
+
+  def to_json(*_args)
+    JSON.generate({class:  author: @author, title: @title })
+  end
+
 end
