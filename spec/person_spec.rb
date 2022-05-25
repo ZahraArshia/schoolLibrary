c9514f1f1a0d5feefb75724eedad4641ae7f2198
ZahraArshia <78906545+ZahraArshia@users.noreply.github.com>
2022-05-25 21:00:51 +0430

    Update person_spec.rb

--- spec/person_spec.rb
+++ spec/person_spec.rb
@@ -22,4 +22,19 @@ describe Person do
       @person.name.should eql 'Name'
     end
   end
+
+  describe '#can_use_services? method test' do
+    it 'Should return true if age >= 18 or parent_permission: true ' do
+      person1 = Person.new(10, 'Name', parent_permission: true)
+      expect(person1.can_use_services?).to be true
+      person2 = Person.new(20, 'Name', parent_permission: false)
+      expect(person2.can_use_services?).to be true
+    end
+  end
+
+  describe '#correct_name' do
+    it 'Should return the name when correct_name is called' do
+      expect(@person.correct_name).to eql 'Name'
+    end
+  end
 end
