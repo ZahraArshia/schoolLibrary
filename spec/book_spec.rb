8c222ef93493fb91e2fa3be545b8b58ca38f24d7
iLynette <lynetteacholah@icloud.com>
2022-05-25 16:59:20 +0300

    Return the correct title

--- spec/book_spec.rb
+++ spec/book_spec.rb
@@ -10,4 +10,10 @@ describe Book do
       @book.should be_an_instance_of Book
     end
   end
+
+  describe '#title' do
+    it 'returns the correct title' do
+      @book.title.should eql 'Title'
+    end
+  end
 end
