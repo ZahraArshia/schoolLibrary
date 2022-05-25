8e63c390ee6affed3275f60288f6f46fafa21ce4
ZahraArshia <78906545+ZahraArshia@users.noreply.github.com>
2022-05-25 20:40:31 +0430

    add test for add_student method

--- spec/classroom_spec.rb
+++ spec/classroom_spec.rb
@@ -16,4 +16,20 @@ describe Classroom do
       @classroom.label.should eql 'Lable'
     end
   end
+
+  describe '#add_student methode test' do
+    it 'Should return two student' do
+      lynette = Student.new(20, 'Lynette', parent_permission: true)
+      zahra = Student.new(25, 'Zahra', parent_permission: false)
+      @classroom.add_student(lynette)
+      @classroom.add_student(zahra)
+      expect(@classroom.students.length).to eql 2
+    end
+
+    it 'Should return the classroom of the student' do
+      zahra = Student.new(25, 'Zahra', parent_permission: true)
+      @classroom.add_student(zahra)
+      expect(zahra.classroom).to eql @classroom
+    end
+  end
 end
--- student.rb
+++ student.rb
@@ -15,6 +15,6 @@ class Student < Person
 
   def classroom=(classroom)
     @classroom = classroom
-    classroom.student.push(self) unless classroom.student.include?(self)
+    classroom.student.push(self) unless classroom.students.include?(self)
   end
 end
